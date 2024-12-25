bc6e8864b2bb9b1d2f91f61c6bbcab8753fb4a7d
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2024-12-25 22:23:37 +0530

    mvc informatic nike web app

--- SpringMvc0100/src/main/java/springmvc/controller/HomeController.java
+++ SpringMvc0100/src/main/java/springmvc/controller/HomeController.java
@@ -5,10 +5,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller 
 public class HomeController {
+	
+	@RequestMapping("/")
+	public String homepage() {
+		return"index";
+	}
 
 	@RequestMapping("/home")
-	public String home() {
+	public String nikehome() {
 		System.out.println("home is running");
-		return "home";
+		return "index";
+	}
+
+	/* SpringMvc0100/about */
+	@RequestMapping("/about")
+	public String abouthome() {
+		return "about";
+		}
+	
+	@RequestMapping("/men")
+	public String menpage() {
+		return "men";
+	}
+	
+	@RequestMapping("/women")
+	public String womenpage() {
+		return "women";
+	}
+	
+	@RequestMapping("/sale")
+	public String salepage() {
+		return "sale";
+	}
+	
+	@RequestMapping("/kids")
+	public String kidspage() {
+		return "kids";
 	}
 }
