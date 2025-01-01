f23e4b36b8c5645ab57e740b1e9b1213801ae26c
Abhishek Aryan <abhishek.aryangiri@gmail.com>
2025-01-01 18:11:22 +0530

    testing

--- SpringMvc0101RegistrationForm/src/main/java/springmvc/controller/FormController.java
+++ SpringMvc0101RegistrationForm/src/main/java/springmvc/controller/FormController.java
@@ -1,6 +1,32 @@
 package springmvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class FormController {
-
+	
+	@RequestMapping("/")
+	public String homepage(Model model) {
+		/* model.addAttribute("msg", "its working or not"); */
+		return "index";
+	}
+	
+	
+	  @RequestMapping("/form") 
+	  public String formHomepage() { 
+		  return "form"; 
+		  }
+	 
+	
+	
+	
+	  @RequestMapping(path = "/formaction", method = RequestMethod.POST) 
+	  public String formHandler() { 
+		  return "succespage"; 
+		  }
+	 
+	 
 }
